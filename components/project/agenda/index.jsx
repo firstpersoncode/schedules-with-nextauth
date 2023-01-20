f5b883ae6cfd2ebf0fef8eb8935dce11c3b0a43a
firstpersoncode <fpcdatadriver@gmail.com>
2023-01-21 01:13:35 +0700

    tidy up agenda's view wrapper

--- components/project/agenda/index.jsx
+++ components/project/agenda/index.jsx
@@ -16,46 +16,42 @@ export default function Agenda() {
 
       <Box
         sx={{
-          display: !isTable && view.value === Views.DAY ? "block" : "none",
           height: "100vh",
           overflowY: "auto",
           pt: "80px",
         }}
       >
-        <DayView />
-      </Box>
+        <Box
+          sx={{
+            display: !isTable && view.value === Views.DAY ? "block" : "none",
+          }}
+        >
+          <DayView />
+        </Box>
 
-      <Box
-        sx={{
-          display: !isTable && view.value === Views.WEEK ? "block" : "none",
-          height: "100vh",
-          overflowY: "auto",
-          pt: "80px",
-        }}
-      >
-        <WeekView />
-      </Box>
+        <Box
+          sx={{
+            display: !isTable && view.value === Views.WEEK ? "block" : "none",
+          }}
+        >
+          <WeekView />
+        </Box>
 
-      <Box
-        sx={{
-          display: !isTable && view.value === Views.MONTH ? "block" : "none",
-          height: "100vh",
-          overflowY: "auto",
-          pt: "80px",
-        }}
-      >
-        <MonthView />
-      </Box>
+        <Box
+          sx={{
+            display: !isTable && view.value === Views.MONTH ? "block" : "none",
+          }}
+        >
+          <MonthView />
+        </Box>
 
-      <Box
-        sx={{
-          display: isTable ? "block" : "none",
-          height: "100vh",
-          overflowY: "auto",
-          pt: "80px",
-        }}
-      >
-        <TableView />
+        <Box
+          sx={{
+            display: isTable ? "block" : "none",
+          }}
+        >
+          <TableView />
+        </Box>
       </Box>
     </>
   );
