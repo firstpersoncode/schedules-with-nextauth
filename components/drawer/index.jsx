ffcb44d74499ac6a78d29b1f1ee117ea33845545
firstpersoncode <fpcdatadriver@gmail.com>
2023-01-31 00:17:07 +0700

    remove close button

--- components/drawer/index.jsx
+++ components/drawer/index.jsx
@@ -1,11 +1,5 @@
 import dynamic from "next/dynamic";
-import { Close } from "@mui/icons-material";
-import {
-  SwipeableDrawer,
-  Drawer as MuiDrawer,
-  IconButton,
-  Box,
-} from "@mui/material";
+import { SwipeableDrawer, Drawer as MuiDrawer } from "@mui/material";
 import { useCommonContext } from "context/common";
 
 const Menu = dynamic(() => import("./menu"));
@@ -33,19 +27,6 @@ export default function Drawer() {
               },
             }}
           >
-            <Box
-              sx={{
-                position: "absolute",
-                top: 0,
-                right: 0,
-                backgroundColor: "#FFF",
-                zIndex: 10000,
-              }}
-            >
-              <IconButton sx={{ p: 1 }} size="small" onClick={toggleDrawer}>
-                <Close />
-              </IconButton>
-            </Box>
             {drawer && <Menu />}
           </SwipeableDrawer>
         </>
--- components/infoDrawer/index.jsx
+++ components/infoDrawer/index.jsx
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
-import { Close } from "@mui/icons-material";
-import { SwipeableDrawer, IconButton, Box } from "@mui/material";
+import { SwipeableDrawer, Box } from "@mui/material";
 import { useCommonContext } from "context/common";
 
 const Notifications = dynamic(() => import("./notifications"));
@@ -23,11 +22,6 @@ export default function Infobar() {
         },
       }}
     >
-      <Box>
-        <IconButton sx={{ p: 1 }} size="small" onClick={toggleInfoDrawer}>
-          <Close />
-        </IconButton>
-      </Box>
       <Box sx={{ overflowY: "auto" }}>{infoDrawer && <Notifications />}</Box>
     </SwipeableDrawer>
   );
